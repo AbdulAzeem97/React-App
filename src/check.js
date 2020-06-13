6ef1f385591d71d93f2b14a953be4f3c570a245e
abdul azeem <30392205+AbdulAzeem97@users.noreply.github.com>
2020-06-13 12:39:06 +0500

    RECHECK

--- src/check.js
+++ src/check.js
@@ -7,7 +7,7 @@ function Checking(props) {
         checking the system {props.newcheck}
         {props.checkagain}{" "}
       </h1>
-      <h2>chandu chandu chandu </h2>
+      <h2>chandu chandu chandu hL</h2>
       {/* <h2>by using surge technology</h2>
       <p1> checking by new component</p1>;<p1> checking by secondcomponent</p1>; */}
     </div>
